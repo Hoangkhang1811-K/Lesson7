9ea52074881493465eeeeb737a833b4c9ae0fec3
unknown <dhkhang18112000.com>
2025-10-15 20:59:59 +0700

    Tinh averages

---
+++ lesson7.js
@@ -0,0 +1,8 @@
+let vatLy=5;
+let hoaHoc=10;
+let sinhHoc=6;
+let average=( vatLy+hoaHoc+sinhHoc)/3
+// prompt("Nhập điểm Vật Lý: "+vatLy);
+// prompt("Nhập điểm Hoá Học: "+hoaHoc);
+// prompt("Nhập điểm Sinh học: "+sinhHoc);
+console.log("Điểm trung bình:"+average);
